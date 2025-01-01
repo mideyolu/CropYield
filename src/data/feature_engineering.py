8fb483f29cea735dbff36d09e9c090a7bb49c0ad
mideyolu <olumideoluwuyi91@gmail.com>
2025-01-01 11:10:50 +0100

    Added feature_engineering.py

--- src/data/feature_engineering.py
+++ src/data/feature_engineering.py
@@ -5,4 +5,29 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def engineer_features (df):
-    pass
+    """
+        Perform feature engineering on the input dataframe, including scaling features.
+
+        Args:
+        - df (pd.DataFrame): The cleaned data.
+
+        Returns:
+        - pd.DataFrame: The data with engineered features.
+    """
+
+    # Scale the features using MinMaxScaler
+    scaler = MinMaxScaler()
+    numerical_features = ['rainfall_mm','soil_quality_index','farm_size_hectares','sunlight_hours','fertilizer_kg','crop_yield']
+
+    #apply scaling
+    df[numerical_features] = scaler.fit_transform(df[numerical_features])
+
+
+    return df
+
+
+# # Test the data cleaning function
+# if __name__ == "__main__":
+#     df = pd.read_csv('data/processed/cleaned_crop_data.csv')
+#     engineered_df = engineer_features(df)
+#     engineered_df.to_csv('data/processed/engineered_crop_data.csv', index=False)
